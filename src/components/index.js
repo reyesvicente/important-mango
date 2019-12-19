a4e0abca0c9abf3d58bc6dbcb3af453041d04800
Vicente G. Reyes <icenreyes@icloud.com>
2019-12-20 00:31:58 +0800

    Update index.js
    removed mailchimp

--- src/components/index.js
+++ src/components/index.js
@@ -49,4 +49,3 @@ export default {
     SubscribeForm,
     Layout
 };
-<script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/0de49a585777f174476066417/924f361c39cd2e50106a4d3fe.js");</script>

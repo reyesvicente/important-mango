a3ed0a40a0f570cceb0b78a1a0b4cc517f9a8b90
Vicente G. Reyes <icenreyes@icloud.com>
2019-12-20 03:06:48 +0800

    Update Layout.js
    wrong

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -21,32 +21,6 @@ export default class Body extends React.Component {
                 </Helmet>
                 <div id="page" className={'site palette-' + _.get(this.props, 'pageContext.site.siteMetadata.palette')}>
                   <Header {...this.props} />
-                      <!-- Load Facebook SDK for JavaScript -->
-                            <div id="fb-root"></div>
-                            <script>
-                              window.fbAsyncInit = function() {
-                                FB.init({
-                                  xfbml            : true,
-                                  version          : 'v5.0'
-                                });
-                              };
-
-                              (function(d, s, id) {
-                              var js, fjs = d.getElementsByTagName(s)[0];
-                              if (d.getElementById(id)) return;
-                              js = d.createElement(s); js.id = id;
-                              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-                              fjs.parentNode.insertBefore(js, fjs);
-                            }(document, 'script', 'facebook-jssdk'));</script>
-
-                            <!-- Your customer chat code -->
-                            <div class="fb-customerchat"
-                              attribution=setup_tool
-                              page_id="110100630343946"
-                        logged_in_greeting="Hi! How can we help you?"
-                        logged_out_greeting="Hi! How can we help you?">
-                            </div>
-                      <!-- End of Facebook SDK for JavaScript-->
                   <main id="content" className="site-content">
                     {this.props.children}
                   </main>

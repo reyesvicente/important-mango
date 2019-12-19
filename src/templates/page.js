31915c1880d1b9efc94db23f29caf3a6257598b2
Vicente G. Reyes <icenreyes@icloud.com>
2019-12-19 23:37:55 +0800

    Update page.js
    removed mailchimp

--- src/templates/page.js
+++ src/templates/page.js
@@ -13,7 +13,6 @@ export default class Page extends React.Component {
                 <article className="post post-full">
                   <header className="post-header">
                     <h1 className="post-title">{_.get(this.props, 'pageContext.frontmatter.title')}</h1>
-                       <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/0de49a585777f174476066417/9a3adea1fbb981e397698fec6.js");</script>
                   </header>
                   {_.get(this.props, 'pageContext.frontmatter.img_path') && 
                   <div className="post-thumbnail">

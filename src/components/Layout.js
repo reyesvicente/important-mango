0a0ef471af97039ac230a0182fd931da9c5eb181
Vicente G. Reyes <icenreyes@icloud.com>
2019-12-20 00:21:48 +0800

    Update Layout.js
    removed mailchimp

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -18,7 +18,6 @@ export default class Body extends React.Component {
                     <meta name="google-site-verification" content="dXb4OOKiaT7_QLCjkrzaZcYH8Omxy8LnMfaG-Zn9trk" />
                     <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,700i" rel="stylesheet"/>
                     <link rel="stylesheet" href={safePrefix('assets/css/main.css')}/>
-                    <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/0de49a585777f174476066417/924f361c39cd2e50106a4d3fe.js");</script>
                 </Helmet>
                 <div id="page" className={'site palette-' + _.get(this.props, 'pageContext.site.siteMetadata.palette')}>
                   <Header {...this.props} />

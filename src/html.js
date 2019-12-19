08f92b4edfa0a43900584ee99e89c22692512001
Vicente G. Reyes <icenreyes@icloud.com>
2019-12-20 03:04:38 +0800

    Update html.js
    pakshet

--- src/html.js
+++ src/html.js
@@ -8,32 +8,6 @@ export default function HTML(props) {
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
-      <!-- Load Facebook SDK for JavaScript -->
-            <div id="fb-root"></div>
-            <script>
-              window.fbAsyncInit = function() {
-                FB.init({
-                  xfbml            : true,
-                  version          : 'v5.0'
-                });
-              };
-
-              (function(d, s, id) {
-              var js, fjs = d.getElementsByTagName(s)[0];
-              if (d.getElementById(id)) return;
-              js = d.createElement(s); js.id = id;
-              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-              fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));</script>
-
-            <!-- Your customer chat code -->
-            <div class="fb-customerchat"
-              attribution=setup_tool
-              page_id="110100630343946"
-        logged_in_greeting="Hi! How can we help you?"
-        logged_out_greeting="Hi! How can we help you?">
-            </div>
-      <!-- End of Facebook SDK for JavaScript-->
         {props.preBodyComponents}
         <noscript key="noscript" id="gatsby-noscript">
           This app works best with JavaScript enabled.

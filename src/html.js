af47aebe62c50c8f580a0ff3b125899027154d6b
Vicente G. Reyes <icenreyes@icloud.com>
2019-12-19 10:00:11 +0800

    Update html.js
    gtag again

--- src/html.js
+++ src/html.js
@@ -6,6 +6,15 @@ export default function HTML(props) {
     <html {...props.htmlAttributes}>
       <head>
         {props.headComponents}
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-154728787-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-154728787-1');
+        </script>
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}

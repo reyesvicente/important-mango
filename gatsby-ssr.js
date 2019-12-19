320ac2ffe36fcaf565a4788033024e08f6167f06
Vicente G. Reyes <icenreyes@icloud.com>
2019-12-20 00:44:14 +0800

    Update gatsby-ssr.js
    removed

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -18,7 +18,6 @@ exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
             <script src={safePrefix('assets/js/plugins.js')}/>
             <script src={safePrefix('assets/js/init.js')}/>
             <script src={safePrefix('assets/js/main.js')}/>
-            <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/0de49a585777f174476066417/7779af4fd225209e4263992ad.js");</script>
         </React.Fragment>
     ]);
 

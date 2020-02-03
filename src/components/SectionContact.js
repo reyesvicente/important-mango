225ae71f7756631cea2bfd48cb5ba59676c14bcd
Vicente G. Reyes <icenreyes@icloud.com>
2020-02-04 03:20:15 +0800

    Update SectionContact.js
    updated form

--- src/components/SectionContact.js
+++ src/components/SectionContact.js
@@ -25,17 +25,61 @@ export default class SectionContact extends React.Component {
                   <p className="screen-reader-text">
                     <label>Don't fill this out if you're human: <input name="bot-field" /></label>
                   </p>
+                  <h2
                   <p className="form-row">
-                    <label className="form-label">Name</label>
-                    <input type="text" name="name" className="form-input" required/>
+                    <label className="form-label">Your Name(required)</label>
+                    <input type="text" name="text" className="form-input" required/>
                   </p>
                   <p className="form-row">
-                    <label className="form-label">Email address</label>
-                    <input type="email" name="email" className="form-input" required/>
+                    <label className="form-label">Your email address(required)</label>
+                    <input type="email" name="email" className="form-input" aria-describedby="emailHelp" required/>
                   </p>
                   <p className="form-row">
-                    <label className="form-label">Message</label>
+                    <label className="form-label">What is the name of your business?(required)</label>
+                    <input type="text" name="text" className="form-input" required/>
+                  </p>
+                  <p className="form-row">
+                    <label className="form-label">Where is your business located?(required)</label>
+                    <input type="text" name="text" className="form-input" required/>
+                  </p>
+                  <p className="form-row">
+                    <label className="form-label">What does your business sell or do?(required)</label>
                     <textarea name="message" className="form-textarea" rows="7" required/>
+                  </p>
+                  <p className="form-row">
+                      <label className="form-label" for="people">How many people work in your company?(required)</label>
+                      <select class="form-control" id="people">
+                      <option>Just Me</option>
+                      <option>2 - 5</option>
+                      <option>6 - 15</option>
+                      <option>16 - 39</option>
+                      <option>40+</option>
+                      </select>
+                  </p>
+                  <p className="form-row">
+                      <label className="form-label" for="typeOfProject">What type of project is it?(required)</label>
+                      <select class="form-control" id="typeOfProject">
+                      <option>Shopify Development</option>
+                      <option>Custom Web Development</option>
+                      <option>WordPress Development</option>
+                      <option>Something Else</option>
+                      </select>
+                  </p>
+                  <p className="form-row">
+                    <label className="form-label">Who is the person responsible for the project?(required)</label>
+                    <input type="text" name="email" className="form-input" required/>
+                  </p>
+                  <p className="form-row">
+                      <label className="form-label" for="budgetOfProject">What is the budget allocated for the project?(required)</label>
+                      <select class="form-control" id="budgetOfProject">
+                      <option>$1500 USD - $3000 USD</option>
+                      <option>$3001 USD - $6000 USD</option>
+                      <option>$6001 USD - $10000 USD</option>
+                      </select>
+                  </p>
+                  <p className="form-row">
+                    <label className="form-label">Deadline for the project?(required)</label>
+                    <input className="form-input" type="date" name="date" placeholder="dd / mm / yyyy" required>
                   </p>
                   <div data-netlify-recaptcha="true"></div>
                   <input type="hidden" name="form-name" value="contactForm" />

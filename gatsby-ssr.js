a76407d8aaf80cc8889b3b4df2be89d067f71c9c
Vicente G. Reyes <icenreyes@icloud.com>
2020-06-19 09:45:47 +0800

    Update gatsby-ssr.js
    instant page

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -19,6 +19,7 @@ exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
             <script src={safePrefix('assets/js/init.js')}/>
             <script src={safePrefix('assets/js/main.js')}/>
             <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/6915103.js"></script>
+            <script src="//instant.page/5.1.0" type="module" integrity="sha384-by67kQnR+pyfy8yWP4kPO12fHKRLHZPfEsiSXR8u2IKcTdxD805MGUXBzVPnkLHw"></script>
         </React.Fragment>
     ]);
 
